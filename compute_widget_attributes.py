0d08bd62249b88bb7f6c53f8e8d98b1e564af81a
jagadesh.S <146544439+sjagaprakash@users.noreply.github.com>
2025-09-25 14:26:16 +0530

    Add files via upload

---
+++ compute_widget_attributes.py
@@ -0,0 +1,74 @@
+
+from typing import Literal
+
+from sentinelx.gui.logged_in import user
+from sentinelx.gui.openapi.framework import (
+    APIVersion,
+    EndpointDoc,
+    EndpointHandler,
+    EndpointMetadata,
+    EndpointPermissions,
+    VersionedEndpoint,
+)
+from sentinelx.gui.openapi.framework.model import api_field, api_model
+from sentinelx.gui.openapi.restful_objects.constructors import domain_type_action_href
+
+from ._family import DASHBOARD_FAMILY
+from ._utils import PERMISSIONS_DASHBOARD
+from .model.type_defs import AnnotatedInfoName
+from .model.widget import determine_widget_filter_used_infos
+from .model.widget_content import WidgetContent
+
+
+@api_model
+class ComputedFilterContext:
+    uses_infos: list[AnnotatedInfoName] = api_field(
+        description=(
+            "A list of info names that the widget content uses. "
+            "This means that the widget can be filtered by these info names."
+        )
+    )
+
+
+@api_model
+class ComputedWidgetSpec:
+    filter_context: ComputedFilterContext = api_field(
+        description="Computed filter context attributes for the widget."
+    )
+
+
+@api_model
+class ComputedWidgetSpecResponse:
+    domainType: Literal["widget-compute"] = api_field(description="The domain type of the object.")
+    value: ComputedWidgetSpec = api_field(description="Computed widget specification attributes")
+
+
+@api_model
+class ComputedWidgetSpecRequest:
+    content: WidgetContent = api_field(description="Widget content to compute attributes for.")
+
+
+def compute_widget_attributes_v1(body: ComputedWidgetSpecRequest) -> ComputedWidgetSpecResponse:
+    """Compute widget specification attributes"""
+    user.need_permission("general.edit_dashboards")
+    widget_config = body.content.to_internal()
+    return ComputedWidgetSpecResponse(
+        domainType="widget-compute",
+        value=ComputedWidgetSpec(
+            filter_context=ComputedFilterContext(
+                uses_infos=determine_widget_filter_used_infos(widget_config)
+            )
+        ),
+    )
+
+
+ENDPOINT_COMPUTE_WIDGET_ATTRIBUTES = VersionedEndpoint(
+    metadata=EndpointMetadata(
+        path=domain_type_action_href(domain_type="dashboard", action="compute-widget-attributes"),
+        link_relation="sentinelx/compute",
+        method="post",
+    ),
+    permissions=EndpointPermissions(required=PERMISSIONS_DASHBOARD),
+    doc=EndpointDoc(family=DASHBOARD_FAMILY.name),
+    versions={APIVersion.UNSTABLE: EndpointHandler(handler=compute_widget_attributes_v1)},
+)
